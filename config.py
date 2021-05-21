dfd01ac8bcdea84bcc2b1ab47784be142f735e03
pentexnyx <loool1912@web.de>
2021-05-21 11:39:19 +0200

    some more stuff

--- config.py
+++ config.py
@@ -6,11 +6,12 @@ import os
 
 PORT = int(os.environ.get('PORT', 5000))
 
-# if using heroku: os.environ['TOKEN']
+# The token @botfather sent you goes here, for example 133769420:AndHereGoe4ssomEL0ngT3xtWoHo0o
+# if using heroku: os.environ['TOKEN'] and you'll have to add your token to Heroku's config vars then
 TOKEN = 'YOUR-TOKEN-GOES-HERE'
 
-# ID of the group you want errors to be posted in, eg. -1001336914977
+# ID of the group you want errors to be posted in, for example -1001336914977
 ERROR_GROUP = -1234
 
-# IDs of users you want to allow certain actions, eg. 1039999701, 1039999702
-VERIFIED_USERS = ( 1039999701,1039999702)
+# IDs of users you want to allow certain actions, for example 1039999701, 1039999702
+VERIFIED_USERS = (1039999701, 1039999702)
--- messages.py
+++ messages.py
@@ -12,3 +12,10 @@ from utils import delay_group, delay_group_button_url, now, delay_group_preview
 def secret_command(update: Update, context: CallbackContext):
     update.message.reply_text("Hey! Seems like you're verified.")
     context.bot.send_message(update.message.chat_id, "The secret ingredient is love <3")
+
+
+def private_not_available(update: Update, context: CallbackContext):
+    update.message.reply_text("Heeeey! Stop!"
+                              "\n\n<b>This is not something I'm looking for.</b>"
+                              "\n\nPlease dn't ask me personal stuff, dude.",
+                              parse_mode=ParseMode.HTML)
